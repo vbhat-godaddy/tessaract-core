bce2c53e24ba91f30e914a2a7af1aa43fe619a3f
aprasad2 <aprasad2@godaddy.com>
2022-04-13 19:34:55 -0700

    Added connector module

--- tesseract_connector/src/tesseract_connector.py
+++ tesseract_connector/src/tesseract_connector.py
@@ -1,9 +1,7 @@
-import sys
 import boto3
 import requests
 import json
-import base64
-import time
+import logging
 
 
 def file_to_string(file_name):
@@ -14,7 +12,7 @@ def file_to_string(file_name):
     content_string = content_string.replace("__ROLE__", sys.argv[2])
     return content_string
 
-def initiate_connection():
+def initiate_connection(role,deploy_secret_id):
     airflow_client = boto3.client("mwaa")
     response = airflow_client.create_cli_token(Name="tesseract-airflow")
     auth_token = response.get("CliToken")
@@ -23,12 +21,24 @@ def initiate_connection():
         web_server=response.get("WebServerHostname")
     )
     print(url)
-    role_dict = {"role_arn": sys.argv[3]}
-    create_connection = "connections add --conn-login " + str(sys.argv[1]) \
-              + " --conn-password " + str(sys.argv[2]) \
-              + " --conn-extra " + json.dumps(role_dict) \
-              + " --conn-type " + "aws" + " --conn_id tesseract_aws_conn"
-    print(create_connection)
+    try:
+        session = boto3.session.Session()
+        secret_client = session.client(service_name='secretsmanager')
+        response = secret_client.get_secret_value(
+            SecretId=deploy_secret_id
+        )
+        secretJSON = json.loads(response["SecretString"])
+
+        role_dict = {"role_arn": role}
+        create_connection = "connections add --conn-login " + secretJSON["AccessKeyId"] \
+          + " --conn-password" + secretJSON["SecretAccessKey"] \
+          + "--conn-extra" + json.dumps(role_dict) \
+          + "--conn-type" + "aws" + "--conn-id tesseract_aws_conn"
+        resp = requests.post(url, data=create_connection, headers=hed)
+        print(resp.__dict__)
+    except BaseException as e:
+        print(e)
+    '''
     try:
         resp = requests.post(url, data="dags unpause tesseract_connection", headers=hed)
         print(resp.__dict__)
@@ -39,14 +49,16 @@ def initiate_connection():
         print(output)
     except BaseException as e:
         print(e)
+    '''
 
 def upload_connection():
-    content_string = file_to_string("scripts/tesseract_connection_base.py")
+    content_string = file_to_string("tesseract_connection_base.py")
     s3_resx = boto3.resource("s3")
-    s3_resx.Object(sys.argv[4], "airflow_environment/dags/tesseract_connection_base.py").put(
-        Body=content_string
-    )
-    time.sleep(60)
+    #s3_resx.Object(sys.argv[4], "airflow_environment/dags/tesseract_connection_base.py").put(
+    #    Body=content_string
+    #)
+    #time.sleep(60)
     initiate_connection()
 
-upload_connection()
+def connector_handler(event, context):
+    initiate_connection(event['role_arn'], event['deploy_secret_id'])
