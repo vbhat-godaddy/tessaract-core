21f8f5cbd3ccd264b759243e5765628f6289749d
aprasad2 <aprasad2@godaddy.com>
2022-04-17 12:43:33 -0700

    fixed base config error

--- infrastructure/vars/var_generator.sh
+++ infrastructure/vars/var_generator.sh
@@ -7,17 +7,17 @@ touch $var_file
 account_id_text="account_id"
 account_id_val=`aws sts get-caller-identity --query "Account" --output text`
 echo "$account_id_text: $account_id_val" >> $var_file
-echo "ACC_ID = $account_id_val" >> $base_file
+echo "ACC_ID = '$account_id_val'" >> $base_file
 team_name_val=`aws ssm get-parameter --name /AdminParams/Team/Name --query "Parameter.Value" --output text`
 echo "team_name: $team_name_val" >> $var_file
-echo "TEAM_VAL = $team_name_val" >> $base_file
+echo "TEAM_VAL = '$team_name_val'" >> $base_file
 portfolio_id_val=`aws servicecatalog list-portfolios | jq '.PortfolioDetails[] | select(.DisplayName == "BigDataAnalytics") | .Id' | tr -d '"'`
 echo "bigdata_portfolio: $portfolio_id_val" >> $var_file
 echo "region: $1" >> $var_file
-echo "ACC_REGION = $1" >> $base_file
+echo "ACC_REGION = '$1'" >> $base_file
 environment_val=`aws ssm get-parameter --name /AdminParams/Team/Environment --query "Parameter.Value" --output text`
 echo "env_val: $environment_val" >> $var_file
-echo "ACC_ENV = $environment_val" >> $base_file
+echo "ACC_ENV = '$environment_val'" >> $base_file
 cat $var_file
 echo "*******"
 cat $base_file
